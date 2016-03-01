a336a194cc9a807e17f5f9d942f1a1c0abe20f5a
German Arcila <germanarcila@gmail.com>
2016-03-01 17:41:16 -0500

    final shakil

--- shakil_the_dog.rb
+++ shakil_the_dog.rb
@@ -7,7 +7,27 @@
  
 # You'll probably want to write other methods, but this 
 # encapsulates the core dog logic
+puts "say something to Shakil"
+
+
 def shakil_the_dog
+	while true do 
+		command_to_shakil = gets.chomp
+		if command_to_shakil == 'woof'
+			puts'WOOF WOOF WOOF'
+		elsif (command_to_shakil == 'shakil stop') || (command_to_shakil == 'Shakil STOP!')
+			puts 'Enjoy your moment of silence '
+		elsif command_to_shakil == 'meow'
+			puts 'woof woof woof woof woof'
+		elsif command_to_shakil.split().include?("treat")
+			puts 'Shakil is waiting for a treat.  Be nice, keep your promise'
+		elsif command_to_shakil == 'go away'
+			break
+		else			
+			puts 'woof'
+		end
+		puts "say something else"
+	end
 end
  
 # Run our method
